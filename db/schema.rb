3bd544c507acac4649279f8759cf397e8b5448cd
Bruno Amaral <bru.amaral88@gmail.com>
2017-08-07 16:47:00 -0300

    video model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807190013) do
+ActiveRecord::Schema.define(version: 20170807194428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,5 +39,20 @@ ActiveRecord::Schema.define(version: 20170807190013) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string   "file"
+    t.float    "frame_rate"
+    t.integer  "width"
+    t.integer  "height"
+    t.string   "enconding"
+    t.float    "length"
+    t.boolean  "audio"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id", using: :btree
+  end
+
   add_foreign_key "carts", "users"
+  add_foreign_key "videos", "users"
 end
