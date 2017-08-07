f767f733a476b924afbdc0ea7a607665f9f3bbec
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-08-07 18:19:26 -0300

    Installed simpleform, included bootstrap in stylesheets and re-designed scss folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'videos/index'
-
-  get 'videos/edit'
-
-  get 'videos/show'
-
-  get 'videos/new'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "videos#index"
 end
