e32df168ac7678209e4ccd8a7c39647d5eefe9c3
Bruno Amaral <bru.amaral88@gmail.com>
2017-08-08 07:24:29 -0300

    Add figaro - Protect my secret data in config/application.yml

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,23 +2,27 @@ class VideosController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
 
+
   def index
     @videos = Video.all
   end
 
   def show
+
   end
 
   def edit
   end
 
   def new
+    @video = Video.new
   end
 
   def update
   end
 
   def create
+     @video = Video.create(post_params)
   end
 
   def destroy
@@ -28,5 +32,9 @@ class VideosController < ApplicationController
 
   def find_video
     @video = Video.find(params[:id])
+  end
+
+  def post_params
+    params.require(:video).permit(:file)
   end
 end
