127c016448bc077c916254c10062095512679b26
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-08-10 19:12:11 -0300

    Configurated mailer in production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   #email simulator
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "lewagon-marketplace-project_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  # Configuration for heroku mailing
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "voyc.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
