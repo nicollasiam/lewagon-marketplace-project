f0b43e363f2b05b6335a74fac5e64367bc02aa1c
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-08-10 16:25:32 -0300

    Added dynamic price. Change on cards inserted/removed from div

--- app/assets/javascripts/carts/payable.js
+++ app/assets/javascripts/carts/payable.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
   $('#buyVideos').on('DOMNodeInserted', function() {
     // Grab all children prices
-    var total = 0;
     var videos = $(this).find('.list-group-item');
 
-    if (videos.length == 0) {
-      $('.price-wrapper h3').text('R$ 0');
-    }
-    else {
-      videos.each(function(_, video) {
-        total += parseFloat($(video).find('.text-right').text().trim());
-      });
+    var total = sumTotalItemsPrice(videos);
 
-      $('.price-wrapper h3').text('R$ ' + total);
-    }
+    $('#total-price').text('R$ ' + total);
   });
+
+  $('#buyVideos').on('DOMNodeRemoved', function(e) {
+    //  Get current total
+    var element = $(e.target);
+    var videos = $(this).find('.list-group-item');
+
+    var total = sumTotalItemsPrice(videos);
+
+    $('#total-price').text('R$ ' + (total - parseFloat(element.find('.text-right').text().trim())));
+  });
+
+
+
+  function sumTotalItemsPrice(domElement) {
+    var total = 0;
+
+    domElement.each(function(_, video) {
+      total += parseFloat($(video).find('.text-right').text().trim());
+    });
+
+    return total;
+  }
 })
